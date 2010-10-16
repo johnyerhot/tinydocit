f928fb53ebc5247fa7eecc40132eed5418196646
John Yerhot <john@yerhot.org>
2010-10-15 18:26:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinyurl::Application.config.secret_token = '166c9aa68251962fe0314b467c7bb9dc2d150d1eedce74dcbee915fb7ad85ddaaaf1ecd287427b1e88eb1f9b9627b17bb94aca801124d7e2a8d41dcc5dff3740'
