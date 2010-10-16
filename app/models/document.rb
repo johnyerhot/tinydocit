f6d342e8a463ccc59508d7d815e035790ca47412
John Yerhot <jyerhot@fsc.follett.com>
2010-10-16 16:02:34 -0500

    added beanstalk and some verbage changes

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,7 +5,7 @@ class Document < ActiveRecord::Base
   before_save :set_key
   before_save :check_key
   
-  validates_attachment_size :pdf, :less_than => 5.megabyte
+  validates_attachment_size :pdf, :less_than => 2.megabyte
   validates_attachment_content_type :pdf, :content_type=>['application/pdf'] 
   
   def set_key
