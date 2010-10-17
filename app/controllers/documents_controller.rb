6438ef073aa082db0630b4208cdd10c71fbf7f9d
John Yerhot <john@yerhot.org>
2010-10-17 13:43:03 -0700

    don't blow up if can' find doc, 404 instead

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -19,10 +19,14 @@ class DocumentsController < ApplicationController
 
   def view
     @document = Document.find_by_key( params[:key] )
-    @pages = @document.fetch_jpg_paths
-    @document.log_viewing unless @pages.empty?
+    if @document
+      @pages = @document.fetch_jpg_paths
+      @document.log_viewing unless @pages.empty?
 
-    render :layout => "view_pdf"
+      render :layout => "view_pdf"
+    else
+      render :status => 404, :file => "public/404.html", :layout => false
+    end
   end
   
   def download
